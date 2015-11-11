9b737dcbbc14ae5a3dc427247aeae65bb366b61d
Kate Wood <katewood611@gmail.com>
2015-11-11 15:47:31 -0800

    creates new user as a variable in app.js

--- app.js
+++ app.js
@@ -10,4 +10,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/family-tree')
 
+// use the model from mongoose
 var User = require('./models/user.js')
+
+var Andy = new User({
+	first_name: "Andy",
+	last_name: "Kim",
+	email: "andykim@gmail.com"
+})
