686d7a7c86b89c282d4b3eed6add4608d9c461a7
Kate Wood <katewood611@gmail.com>
2015-11-11 15:35:02 -0800

    sets up app.js to connect to node modules

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+	logger	= require('morgan'),
+	bodyParser = require('body-parser'),
+	app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/family-tree')
